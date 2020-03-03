de64c56fb3e63c44121ddadb5221711a78447cea
outstandingboy <outstandingboy@kakao.com>
2020-03-03 12:55:28 +0900

    Updates

---
+++ precache-manifest.1b4b35dc81f0ef2ce4f142ed691f8268.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca97e0caa3cb7e9edca5f86c61f23ddc",
+    "url": "/size-table-generator/index.html"
+  },
+  {
+    "revision": "6b0e12da4171b1bc7869",
+    "url": "/size-table-generator/static/css/main.d9bd8ede.chunk.css"
+  },
+  {
+    "revision": "3aaa15891af35d820add",
+    "url": "/size-table-generator/static/js/2.c0b67c6a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/size-table-generator/static/js/2.c0b67c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b0e12da4171b1bc7869",
+    "url": "/size-table-generator/static/js/main.3ad22a45.chunk.js"
+  },
+  {
+    "revision": "5c22dee84f898b643289",
+    "url": "/size-table-generator/static/js/runtime-main.2a890dd5.js"
+  }
+]);
